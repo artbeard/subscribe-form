f3d2fcf46aaace78b2aabc391459b93c9bd9cbdc
artbeard <frolov.web@gmail.com>
2023-02-07 23:14:00 +0600

    Установка зависимостей, очистка от мусора

---
+++ src/components/SubscribeForm.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		форма подписки
+	</div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+	name: 'SubscribeForm'
+})
+</script>
+<style lang="">
+	
+</style>
