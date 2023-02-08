696eea96d4896f17f20d15af7f00b369bb8c3547
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-08 11:19:42 +0600

    Добавлен переключатель для отображения формы

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <template>
-	<SubscribeForm />
+	<button type="button" @click="toggleVisibilityForm">subscribe to news</button>
+	<SubscribeForm
+		v-if="statsusForm"
+		@toggle-visibility="toggleVisibilityForm"
+		 />
 </template>
 <script setup lang="ts">
-import 'primevue/resources/themes/tailwind-light/theme.css'
-import 'primevue/resources/primevue.min.css'
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+// import 'primevue/resources/themes/tailwind-light/theme.css'
+// import 'primevue/resources/primevue.min.css'
+import '../public/css/style.min.css'
+
 import SubscribeForm from './components/SubscribeForm.vue';
+const statsusForm: Ref<boolean> = ref<boolean>(false);
+const toggleVisibilityForm = function()
+{
+	statsusForm.value = !statsusForm.value;
+}
 </script>
 <style scoped>
 </style>
