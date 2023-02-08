36eed02e2f4adb9ee56e2751dbb29d871d3e6289
artbeard <76746834+artbeard@users.noreply.github.com>
2023-02-08 12:10:16 +0600

    Перенесен импорт стилей

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import type { Ref } from 'vue'
-// import 'primevue/resources/themes/tailwind-light/theme.css'
-// import 'primevue/resources/primevue.min.css'
-import '../public/css/style.min.css'
-
 import SubscribeForm from './components/SubscribeForm.vue';
 const statsusForm: Ref<boolean> = ref<boolean>(false);
 const toggleVisibilityForm = function()
